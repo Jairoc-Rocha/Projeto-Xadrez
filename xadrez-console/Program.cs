1461d02de68fd7af7923215e3df69262b2fcc447
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-03-27 21:23:35 -0300

    Controlando as peças capturadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -43,13 +43,8 @@ namespace xadrez_console
                 {
                     try
                     {
-
-
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.Turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.ImprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
